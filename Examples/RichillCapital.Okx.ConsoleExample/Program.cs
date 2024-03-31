d7f0510267d00c7a4f835dfcc09d719d6ef53f53
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-03-31 13:46:43 +0800

    GetServerTime

---
+++ Examples/RichillCapital.Okx.ConsoleExample/Program.cs
@@ -0,0 +1,10 @@
+﻿using RichillCapital.Okx;
+
+var apiKey = "your-api-key";
+var secretKey = "your-secret";
+var passphrase = "your-passphrase";
+
+var client = new OkxClient(apiKey, secretKey, passphrase);
+
+var serverTime = await client.GetServerTimeAsync();
+Console.WriteLine($"{serverTime:yyyy-MM-dd HH:mm:ss.fff}");
--- RichillCapital.Okx/OkxClient.cs
+++ RichillCapital.Okx/OkxClient.cs
@@ -1,4 +1,6 @@
-﻿namespace RichillCapital.Okx;
+﻿using Newtonsoft.Json;
+
+namespace RichillCapital.Okx;
 
 public sealed class OkxClient
 {
@@ -8,8 +10,8 @@ public sealed class OkxClient
     private readonly HttpClient _httpClient;
 
     public OkxClient(
-        string apiKey, 
-        string secretKey, 
+        string apiKey,
+        string secretKey,
         string passphrase)
     {
         _httpClient = new HttpClient
@@ -17,4 +19,53 @@ public sealed class OkxClient
             BaseAddress = new Uri(RestUrl),
         };
     }
+
+    public async Task<DateTimeOffset> GetServerTimeAsync()
+    {
+        var response = await _httpClient.GetAsync(ApiRoutes.Public.Time.Get);
+
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        var result = JsonConvert.DeserializeObject<OkxResponse<ServerTimeResponse>>(content);
+
+        var timestamp = result!.Data.First().Timestamp;
+
+        return DateTimeOffset.FromUnixTimeMilliseconds(timestamp);
+    }
+}
+
+internal static class ApiRoutes
+{
+    private const string ApiBase = "api/v5";
+
+    internal static class Public
+    {
+        private const string PublicBase = $"{ApiBase}/public";
+
+        internal static class Time
+        {
+            private const string TimeBase = $"{PublicBase}/time";
+            internal const string Get = TimeBase;
+        }
+    }
+}
+
+public sealed record OkxResponse<TData>
+{
+    [JsonProperty("code")]
+    public int Code { get; init; }
+
+    [JsonProperty("msg")]
+    public string Message { get; init; } = string.Empty;
+
+    [JsonProperty("data")]
+    public IEnumerable<TData> Data { get; init; } = default!;
+}
+
+public sealed record ServerTimeResponse
+{
+    [JsonProperty("ts")]
+    public long Timestamp { get; init; }
 }
